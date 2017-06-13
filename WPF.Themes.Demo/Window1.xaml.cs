b36f3a73804110428573ef1e6f93a8ce0c7289de
Michele Cattafesta <michele.cattafesta@mesta-automation.com>
2017-06-13 23:32:47 +0100

    1st commit.
    Signed-off-by: Michele Cattafesta <michele.cattafesta@mesta-automation.com>

---
+++ WPF.Themes.Demo/Window1.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace WPF.Themes.Demo
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        
+        
+
+        
+        List<Person> SouthPark = new List<Person>() { 
+            new Person() { Name = "Eric", Surname="Cartman" },
+            new Person() { Name = "Stan", Surname="Marsh" },
+            new Person() { Name = "Kyle", Surname="Broflovski" },
+            new Person() { Name = "Kenny", Surname="McCormick" },
+            new Person() { Name = "Bebe", Surname="Stevens" },
+            new Person() { Name = "Clyde", Surname="Donovan" }, 
+            new Person() { Name = "Craig", Surname="Tucker" }, 
+            new Person() { Name = "Jimmy", Surname="Vulmer" }, 
+            new Person() { Name = "Pip", Surname="Pirrup" }, 
+            new Person() { Name = "Token", Surname="Black" }, 
+            new Person() { Name = "Tweek", Surname="Tweak" }, 
+            new Person() { Name = "Wendy", Surname="Testaburger" }, 
+            new Person() { Name = "Annie", Surname="Polk" },
+            new Person() { Name = "Randy", Surname="Marsh" },
+            new Person() { Name = "Sharon", Surname="Marsh" },
+            new Person() { Name = "Shelley", Surname="Marsh" },
+            new Person() { Name = "Marvin", Surname="Marsh" },
+            new Person() { Name = "Jimbo", Surname="Kern" },
+            new Person() { Name = "Gerald", Surname="Broflovski" },
+            new Person() { Name = "Sheila", Surname="Broflovski" },
+            new Person() { Name = "Ike", Surname="Broflovski" },
+            new Person() { Name = "Kyle", Surname="Schwartz" },
+            new Person() { Name = "Liane", Surname="Cartman" },
+            new Person() { Name = "Stuart", Surname="McCormick" },
+            new Person() { Name = "Carol", Surname="McCormick" },
+            new Person() { Name = "Kevin", Surname="McCormick" },
+            new Person() { Name = "Stephen", Surname="Stotch" },
+            new Person() { Name = "Linda", Surname="Stotch" },
+            new Person() { Name = "Richard", Surname="Tweak" }
+        };
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            themes.ItemsSource = ThemeManager.GetThemes();
+
+            comboBox.ItemsSource = SouthPark;
+            listBox.ItemsSource = SouthPark;
+        }
+
+        private void themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                string theme = e.AddedItems[0].ToString() ;
+
+                // Window Level
+                // this.ApplyTheme(theme);
+
+                // Application Level
+                // Application.Current.ApplyTheme(theme);
+            }
+        }
+    }
+}
